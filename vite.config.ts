fe3dd20a421b881f534e22964dead61ac3ea4319
Malik Heron <malik.heron2001@gmail.com>
2023-05-12 11:22:22 -0500

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { watch: { awaitWriteFinish: true } },
-  base: `https://malikheron.github.io/Website-Portfolio-React/`,
+  base: `https://github.com/MalikHeron/Website-Portfolio-React/`,
   plugins: [react()],
 });
