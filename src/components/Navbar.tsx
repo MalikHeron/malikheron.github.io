80e02cafb7370606650d492fdd422798e78707ba
Malik Heron <malik.heron2001@gmail.com>
2024-02-04 02:52:15 -0500

    Update Navbar and responsive styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -53,7 +53,7 @@ function Navbar() {
       btn.click()
       setTimeout(() => {
          handleClick(id)
-      }, 600);
+      }, 400);
    }
 
    return (
@@ -65,27 +65,21 @@ function Navbar() {
             <li id="projectsTab" className='tab' onClick={() => handleClick("projects")}>PROJECTS</li>
             <li id="contactTab" className='tab' onClick={() => handleClick("contact")}>CONTACT</li>
          </nav>
-         <nav className="navbar fixed-top bg-body-tertiary">
+         <nav className="navbar sticky-top bg-body-tertiary">
             <div className="container-fluid">
-               <a href="#home" className="navbar-brand align-text-center">
-                  <img src="assets/logo_fire.png" alt="Logo" width="40" height="40" className="d-inline-block align-text-top" />
+               <a href="#home" className="navbar-brand">
+                  <img src="assets/logo_fire.png" alt="Logo" width="40" height="40"/>
                </a>
-               <button className="navbar-toggler x" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
                   <span className="navbar-toggler-icon"></span>
                </button>
-               <div className="offcanvas offcanvas-end text-bg-dark" tabIndex={-1} id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-                  <div className="offcanvas-header">
-                     <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Malik Heron</h5>
-                     <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                  </div>
-                  <div className="offcanvas-body">
-                     <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li className='nav-link' onClick={() => toggleNavbar("about")}>About</li>
-                        <li className='nav-link' onClick={() => toggleNavbar("experience")}>Experience</li>
-                        <li className='nav-link' onClick={() => toggleNavbar("projects")}>Projects</li>
-                        <li className='nav-link' onClick={() => toggleNavbar("contact")}>Contact</li>
-                     </ul>
-                  </div>
+               <div className="collapse navbar-collapse" id="navbarToggler">
+                  <ul className="navbar-nav">
+                     <li className='nav-link' onClick={() => toggleNavbar("about")}>About</li>
+                     <li className='nav-link' onClick={() => toggleNavbar("experience")}>Experience</li>
+                     <li className='nav-link' onClick={() => toggleNavbar("projects")}>Projects</li>
+                     <li className='nav-link' onClick={() => toggleNavbar("contact")}>Contact</li>
+                  </ul>
                </div>
             </div>
          </nav>
