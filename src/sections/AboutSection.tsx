3ab47c1fa1f07f15aab28779ad07918d9707e7dc
Malik Heron <malik.heron2001@gmail.com>
2024-02-03 21:24:04 -0500

    Update styles and UI design

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -8,13 +8,16 @@ function AboutSection() {
             As a passionate problem-solver, I thrive on the challenge of
             crafting efficient and functional code. My goal is to create a
             seamless user experience through modern and visually
-            appealing UI design. I believe that it is the little details that keep
-            users coming back to apps and websites time and time again.
+            appealing UI design.
             <br /><br />
             With experience in a variety of languages and tools, I am always
-            looking for new ways to work smarter and more efficiently. While
-            my focus is currently on a few key areas, I am excited to continue
-            expanding my skills and knowledge in the future.
+            looking for new ways to work smarter and more efficiently. 
+            I am currently focused on mobile and web development, while
+            continuously expanding my knowledge in UX and UI design. 
+            <br /><br />
+            I believe that having a strong understanding of user experience
+            and user interface design is crucial in creating successful and
+            user-friendly applications. 
          </p>
       </section>
    )
